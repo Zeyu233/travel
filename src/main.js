3f2a45c82f56cba22b946893afdafcaf072421f9
zy <867624339@qq.com>
2021-02-12 17:26:37 +0800

    '项目初始化'

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 点击延迟300ms问题解决
+import fastClick from 'fastclick'
+// 重置样式表
+import './assets/styles/reset.css'
+// 1像素问题，移动端某些机型分辨率高，写的1像素会变大
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
